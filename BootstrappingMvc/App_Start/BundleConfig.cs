22a7f118f38d6910595b5d0772ca51eaa53e0b31
James Chambers <jamesRemove-Spam@jameschambers.com>
2012-07-08 00:12:23 -0500

    Initial Commit
    The project in its infancy...

---
+++ BootstrappingMvc/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace BootstrappingMvc
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/Bootstrap").Include(
+                        "~/Scripts/jquery-1.*",
+                        "~/Scripts/Bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/Bootstrap").Include(
+                        "~/Content/Bootstrap.min.css",
+                        "~/Content/Bootstrap-responsive.css"
+                        ));
+
+        }
+    }
+}
---
+++ BootstrappingMvc/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="jquery-1.7.2.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="jquery.validate.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+/// <reference path="knockout-2.0.0.debug.js" />
+/// <reference path="modernizr-2.0.6-development-only.js" />
