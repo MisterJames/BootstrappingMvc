a56255ee11cec00da83253e8c4e6ea26e4ad9133
James Chambers <james@jameschambers.com>
2012-07-09 08:39:33 -0500

    Adding support for Boolean control templates
    This update replaces the default rendering of the bool type as a model
    property with radio buttons from Bootstrap.

---
+++ BootstrappingMvc/Controllers/MoviePassController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BootstrappingMvc.Models;
+
+namespace BootstrappingMvc.Controllers
+{
+    public class MoviePassController : Controller
+    {
+        //
+        // GET: /MoviePass/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /MoviePass/Details/5
+
+        public ActionResult Details(int? id)
+        {
+            MoviePass pass = new MoviePass
+            {
+                MovieTitle = "The Last Starfighter",
+                CanUseForNewReleases = true,
+                CanSkipLine = false
+            };
+
+            return View(pass);
+        }
+
+        //
+        // GET: /MoviePass/Create
+
+        public ActionResult Create()
+        {
+            MoviePass pass = new MoviePass
+            {
+                CanUseForNewReleases = true,
+                CanSkipLine = false
+            };
+
+            return View(pass);
+        }
+
+        //
+        // POST: /MoviePass/Create
+
+        [HttpPost]
+        public ActionResult Create(MoviePass moviePass)
+        {
+            return View(moviePass);
+        }
+
+        //
+        // GET: /MoviePass/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /MoviePass/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /MoviePass/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /MoviePass/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BootstrappingMvc/Models/MoviePass.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BootstrappingMvc.Models
+{
+    public class MoviePass
+    {
+        public int MoviePassId { get; set; }
+
+        [Display(Name = "Movie Title")]
+        public string MovieTitle { get; set; }
+
+        [Display(Name = "Good for New Releases")]
+        public bool? CanUseForNewReleases { get; set; }
+
+        [Display(Name = "VIP - Skip the Line")]
+        public bool CanSkipLine { get; set; }
+
+    }
+}
